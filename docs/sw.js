02c8ba136da719696bcf2f5b135adf5745baae5a
Teodor Lagerqvist <git@teodor.se>
2020-07-01 22:13:02 +0200

    Precach and bump

--- docs/sw.js
+++ docs/sw.js
@@ -14,23 +14,23 @@ limitations under the License.
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v0.0.33';
+const PRECACHE = 'precache-v0.0.34';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  // './',
-  // 'favicon.ico',
-  // 'favicon-192x192.png',
-  // 'favicon-512x512.png',
-  // 'icons.svg',
-  // 'index.html',
-  // 'manifest.json',
-  // 'js/main.js',
-  // 'js/solver-worker.js',
-  // 'css/style.css',
-  // 'css/dialog.css',
-  // 'css/button.css',
+  './',
+  'favicon.ico',
+  'favicon-192x192.png',
+  'favicon-512x512.png',
+  'icons.svg',
+  'index.html',
+  'manifest.json',
+  'js/main.js',
+  'js/solver-worker.js',
+  'css/style.css',
+  'css/dialog.css',
+  'css/button.css',
 ];
 
 // The install handler takes care of precaching the resources we always need.
